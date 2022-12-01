9dbaf68dadea1a7280643e7e4227c59d1cd0074a
Gabo <gsarmiento101874@gmail.com>
2022-12-01 00:31:28 -0500

    Added logo and fixed navbar

--- couparking/settings.py
+++ couparking/settings.py
@@ -129,8 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_FILES_DIRS = [STATIC_DIR,]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 #  MEDIA
 

75732a6d91c815f13dcf69adb54c194d0dd94589
gabo963 <gsarmiento101874@gmail.com>
2022-12-01 20:36:05 -0500

    Created Models and started working on Forms

--- coupark/forms.py
+++ coupark/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from coupark.models import UserProfileInfo
+import re
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -8,9 +9,29 @@ class UserForm(forms.ModelForm):
     class Meta():
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password')
+    
+    def clean_mail(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError(u'Email addresses must be unique.')
+
+        return email
+
+    def company_mail(self):
+
+        email = self.cleaned_data.get('email')
+        
+        regex = r'\b[A-Za-z0-9._%+-]+@coupa.com'
+
+        if (re.fullmatch(regex, email)):
+            raise forms.ValidationError(u'Email addresses must belong to Coupa.')
+
+        return email
+
 
 class UserProfileInfoForm(forms.ModelForm):
 
     class Meta():
         model = UserProfileInfo
-        fields = ('portfolio_site', 'profile_pic')
+        fields = ('profile_pic')
--- coupark/models.py
+++ coupark/models.py
@@ -1,5 +1,8 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from datetime import datetime
 # Create your models here.
 
 class UserProfileInfo( models.Model ):
@@ -7,8 +10,39 @@ class UserProfileInfo( models.Model ):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     #  Additional classes
-    portfolio_site = models.URLField( blank=True )
     profile_pic = models.ImageField( upload_to='profile_pics', blank = True )
 
     def __str__(self):
         return self.user.username
+
+def validate_ParkingSpace_vehicle_type(value):
+    if not (vehicleType == 'Car' or vehicleType == 'Motorbike'):
+        raise forms.ValidationError("Vehicle Type not allowed")
+
+class ParkingSpace( models.Model ):
+    
+    name = models.CharField( max_length=15 )
+    description = models.CharField( max_length=100 )
+    active = models.BooleanField( default = True )
+    vehicleType = models.CharField(max_length=15, validators=[validate_ParkingSpace_vehicle_type])
+
+    
+    def __str__(self):
+        return self.name
+
+class Date( models.Model ):
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
+
+class ParkingReservation( models.Model ):
+
+    parkingSpace = models.ForeignKey(ParkingSpace, on_delete=models.CASCADE)
+    user = models.ForeignKey( User, on_delete=models.CASCADE )
+    date = models.ForeignKey( Date, on_delete=models.CASCADE )
+
+    def __str__(self):
+        return self.user.__str__ + ' ' +  self.parkingSpace.__str__ + ' ' + self.date.__str__
+
+

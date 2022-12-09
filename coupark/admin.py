7fa093ee4b17b42e983e06ff0c60a32b8805d84e
Gabo <gsarmiento101874@gmail.com>
2022-12-08 22:34:27 -0500

    added models to admin

--- coupark/admin.py
+++ coupark/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from coupark.models import UserProfileInfo
+from coupark.models import UserProfileInfo, ParkingSpace, ParkingReservation, Date
 
 # Register your models here.
 admin.site.register(UserProfileInfo)
+admin.site.register(ParkingSpace)
+admin.site.register(ParkingReservation)
+admin.site.register(Date)

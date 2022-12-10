700ba04728b628d4c041e1956a03a63c29cc8a94
Gabo <gsarmiento101874@gmail.com>
2022-12-09 23:58:15 -0500

    spaces

--- couparking/settings.py
+++ couparking/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'templates'
 STATIC_DIR = BASE_DIR / 'static'
+STATIC_DIR = BASE_DIR / 'static'
 MEDIA_DIR = BASE_DIR / 'media'
 
 

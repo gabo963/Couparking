640096ab37ff85f2434cf8dbf75ff34d56d2cd5a
Gabriel Sarmiento <gabriel.sarmiento@coupa.com>
2022-12-19 09:54:33 -0500

    added command new day to create new entries

---
+++ coupark/management/commands/newDay.py
@@ -0,0 +1,62 @@
+from django.core.management.base import BaseCommand
+import logging
+logger = logging.getLogger(__name__)
+
+from coupark.models import ParkingSpace, ParkingReservation, Date
+from datetime import datetime, timedelta
+
+# python manage.py newDay --mode=refresh
+
+""" Clear all data and creates addresses """
+MODE_REFRESH = 'refresh'
+
+class Command(BaseCommand):
+    help = "Create new reservation entries for a new day."
+
+    def add_arguments(self, parser):
+        parser.add_argument('--mode', type=str, help="Mode")
+
+    def handle(self, *args, **options):
+        self.stdout.write('Creating new reservation entries...')
+        run_newDay(self, options['mode'])
+        self.stdout.write('done.')
+
+def NewDay():
+    """
+    Adds a new day
+    if tomorrow is not created.
+    """
+
+    nextDate = datetime.today() + timedelta(days=1)
+
+    if Date.objects.last().date != nextDate:
+
+        parkingLots = ParkingSpace.objects.all()
+
+        Date.objects.update_or_create( date = nextDate, defaults={'date':nextDate} )
+
+        dbDate = Date.objects.get(date = nextDate)
+
+        for parkingLot in parkingLots:
+            if parkingLot.active:
+                # Update or create new item to Parking reservation
+                ParkingReservation.objects.update_or_create( date = dbDate, parkingSpace = parkingLot, defaults={'user':None, 'date':dbDate, 'parkingSpace':parkingLot})
+        
+        logger.info("Next Day created")
+
+    else:
+      logger.info("Next Day already created")
+
+def run_newDay(self, mode):
+    """ Create a NewDay database
+
+    :param mode: refresh 
+    :return:
+    """
+
+    if mode == 'refresh':
+            
+        # Adds a newDay of reservation entries
+        newDay()
+
+

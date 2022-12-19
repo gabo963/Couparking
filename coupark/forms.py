9b8a8224412879fdf73bac5a5aa036f08bcc65ed
Gabriel Sarmiento <gabriel.sarmiento@coupa.com>
2022-12-19 10:04:31 -0500

    removed email validation

--- coupark/forms.py
+++ coupark/forms.py
@@ -2,17 +2,17 @@ from django import forms
 from django.contrib.auth.models import User
 import re
 
-def company_mail(email):
+# def company_mail(email):
  
-    regex = r'\b[A-Za-z0-9._%+-]+@coupa.com'
+#     regex = r'\b[A-Za-z0-9._%+-]+@coupa.com'
 
-    if (not re.fullmatch(regex, email)):
-        raise forms.ValidationError(u'Email addresses must belong to Coupa.')
+#     if (not re.fullmatch(regex, email)):
+#         raise forms.ValidationError(u'Email addresses must belong to Coupa.')
 
 class UserForm(forms.ModelForm):
 
     password = forms.CharField(widget=forms.PasswordInput())
-    email = forms.EmailField(validators=[company_mail])
+    email = forms.EmailField()
 
     class Meta:
         model = User
